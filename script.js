88b80dde30f5c3c14ab07cb0bf237e9ad377a91b
Cristiane Hdo <cristiane.hdo@gmail.com>
2017-05-28 11:48:09 -0300

    script woking next step calculate line  position

--- script.js
+++ script.js
@@ -3,15 +3,26 @@ TABLEHEIGHT = 720;
 COLBORDER = 1;
 COLNUMBER = 18;
 LINENUMBER = 9;
-// CLASSES = {};
 
-function createGrid(){
-	var width = TABLEWIDTH / (COLNUMBER + COLBORDER);
-	var heigth = TABLEHEIGHT / (LINENUMBER + COLBORDER);
-	for (var i = 1; i <= COLNUMBER; i++){
-		CLASSES["col"+ i ] = {"width" :  width + ";", "heigth" : heigth + ";"};
+
+$(document).ready(function(){ 
+	function createGrid(){
+		var width = Math.round(TABLEWIDTH / (COLNUMBER + COLBORDER));
+		var heigth = Math.round(TABLEHEIGHT / (LINENUMBER + COLBORDER));
+		var classes = {};
+		for (var i = 0; i < COLNUMBER; i++){
+			var position = i * (width + COLBORDER);
+			classes["col"+ (i+1) ] = {
+				"width" :  width + "px;", 
+				"heigth" : heigth + "px;", 
+				"position" : "relative;",
+				"left" : position +"px;"
+			};
+		}
+		classes = JSON.stringify(classes, null, "    ").replace(/[\"\,]/g, "").replace(/col/g, ".col").replace(/\:\s\{/g, " {");
+		$("#textArea").text(classes);
+
 	}
+	createGrid();
 
-}
-
-createGrid();
+});
